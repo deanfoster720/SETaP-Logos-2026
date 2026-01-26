fb0c8cafd5b3c63e7648cedaa6b4ef0bdd964714
Michael Parker <54761507+micfun123@users.noreply.github.com>
2026-01-26 15:40:14 +0000

    Add turtle graphics code to logo.py

---
+++ logo.py
@@ -0,0 +1,22 @@
+import turtle # Import the turtle module
+
+t = turtle.Turtle() 
+
+t.color("blue")
+t.pensize(3)
+t.right(90)
+t.forward(70)
+t.circle(100,180) 
+t.forward(70)
+
+t.right(180)
+t.forward(170)
+t.right(180)
+t.forward(85)
+t.right(90)
+t.forward(85)
+t.right(180)
+t.forward(80)
+t.right(90)
+
+input()
